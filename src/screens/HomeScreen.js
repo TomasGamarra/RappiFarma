d0f0765ad7e24655fa29ca0af26867657acef41f
Santiago Robaldi <santiagorobaldi@gmail.com>
2025-10-22 19:04:08 -0300

    creando carpetas

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bienvenido a RappiFarma</Text>
+      <Button
+        title="Ir al carrito"
+        onPress={() => navigation.navigate('Cart')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
